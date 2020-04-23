9dcb62ccb438479eaf45fb35e3bcfea7c5d843bb
jake waters <jerkey@g-cipher.net>
2020-04-22 22:17:53 -0700

    three-channel datalogger works

--- analogReadThree.ino
+++ analogReadThree.ino
@@ -1,37 +1,42 @@
+#define VOLTAGE_PIN A0
+#define CHARGE_PIN A1
+#define DISCHARGE_PIN A2
+
+#define PRINT_TIME 1000
+
+uint32_t lastPrintTime = 0;
+
 void setup() {
   Serial.begin(57600);
+  lastPrintTime = millis();
 }
 
-#define SHRINK 35
+uint32_t voltage, charge, discharge;
+uint32_t voltageAdder, chargeAdder, dischargeAdder;
+uint16_t analogAdds = 0;
+
+void updateAnalogs() { // divide analog oversamplers to get precise values
+  voltage = voltageAdder / analogAdds;
+  charge = chargeAdder / analogAdds;
+  discharge = dischargeAdder / analogAdds;
+  chargeAdder = 0;
+  voltageAdder = 0;
+  dischargeAdder = 0;
+  analogAdds = 0;
+}
+
+void readAnalogs() {
+  voltageAdder += analogRead(VOLTAGE_PIN);
+  chargeAdder += analogRead(CHARGE_PIN);
+  dischargeAdder += analogRead(DISCHARGE_PIN);
+  analogAdds++;
+}
 
 void loop() {
-  digitalWrite(13,digitalRead(2)); // make LED follow D2
-  int a13 = analogRead(A0);
-  Serial.print(a13);
-  Serial.print(" ");
-  for (int i = 0 ; i < SHRINK ; i++) {
-    if ((a13 / SHRINK) > i) Serial.print("#");
-      else Serial.print(" ");
+  readAnalogs();
+  if (millis() - lastPrintTime > PRINT_TIME) {
+    updateAnalogs();
+    Serial.println(String(voltage)+','+String(charge)+','+String(discharge));
+    lastPrintTime = millis();
   }
-  Serial.print(" ");
-  int a14 = analogRead(A1);
-  Serial.print(a14);
-  Serial.print(" ");
-  for (int i = 0 ; i < SHRINK ; i++) {
-    if ((a14 / SHRINK) > i) Serial.print("#");
-      else Serial.print(" ");
-  }
-  Serial.print(" ");
-  int a15 = analogRead(A2);
-  Serial.print(a15);
-  Serial.print(" ");
-  for (int i = 0 ; i < SHRINK ; i++) {
-    if ((a15 / SHRINK) > i) Serial.print("#");
-      else Serial.print(" ");
-  }
-  Serial.print(" ");
-  Serial.print(digitalRead(A3));
-  Serial.print(" ");
-  Serial.print(digitalRead(A4));
-  Serial.println("");    
 }
