747d70ee48a3463c0b1c28c58970adaaa62fdb26
jake waters <jerkey@g-cipher.net>
2020-05-10 23:33:55 -0700

    put the date and time into the beginning of the log

--- readserialline.py
+++ readserialline.py
@@ -2,8 +2,10 @@
 
 import serial
 import sys
+import datetime
 
 p = serial.Serial("/dev/serial/by-id/usb-Arduino_LLC_Arduino_Micro-if00", baudrate=57600)
 
+sys.stdout.write("Start time: "+datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')+'\n')
 while(1):
-        sys.stdout.write(p.readline())
+    sys.stdout.write(p.readline())
